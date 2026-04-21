5e4b1028434293d35e82fcdc9faca7b5161e3382
David <david.wies1@gmail.com>
2026-04-21 08:51:16 +0300

    Remove old main.cpp files and continue config
    - Delete old .continue/agents/new-config.yaml (obsolete agent config)
    - Delete old src/collector/main.cpp and src/gui/main.cpp (now unified in src/main.cpp)

--- src/collector/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include <QDebug>
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication app(argc, argv);
-    qDebug() << "Classic Books collector process started.";
-    return 0;
-}
